04ea658ccd6360202d4b425ca106cb39d6f0ece3
Abdallah Mahmoud <javabyabdallah@gmail.com>
2024-11-24 14:53:56 +0300

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import FileUploader from './components/FileUploader';
+
+export default function App() {
+  return (
+    <div className="mx-auto flex min-h-screen  max-w-7xl flex-col items-center justify-center p-5">
+      <FileUploader />
+    </div>
+  );
+}
